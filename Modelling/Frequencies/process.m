55b05d2b67f408593264c0487b2e8d7a900bd816
Jonathan Holt <jonathanjamesholt@gmail.com>
2022-02-19 11:33:34 +0000

    Some processing

--- Modelling/Frequencies/process.m
+++ Modelling/Frequencies/process.m
@@ -29,21 +29,25 @@ maxima_u1 = zeros(1,length(frequencies));
 maxima_u2 = zeros(1,length(frequencies));
 maxima_u3 = zeros(1,length(frequencies));
 maxima_u1_u2_u3 = zeros(1,length(frequencies));
-% collected_data = collected_data(2:end);
+u_3 = [];
 for i = 1:numel(collected_data)
-    datum = collected_data{i};
+    datum = collected_data{i};                          %Everything up to here is to undo the choices in python. Produce just one .csv instead, dumdum
     magnitude = sqrt(sum(datum(:, 2:4).^2, 2));
     maxima_u1_u2_u3(i) = max(magnitude);
     maxima_u1(i) = max(abs(datum(:,2)));
     maxima_u2(i) = max(abs(datum(:,3)));
     maxima_u3(i) = max(abs(datum(:,4)));
-
+    if frequencies(i) == 100
+        u_3 = horzcat(datum(:,1), datum(:,4));
+    end
 end
+u_3(u_3 < 1e-11) = 0;
 scatter(frequencies, maxima_u1_u2_u3)
 set(gca, 'yscale', 'log')
 xlabel("Frequency [Hz]"); ylabel("Maximum displacement [m]")
 title("Maximum total displacement")
-writematrix(horzcat(frequencies', maxima_u1_u2_u3'), "u1_u2_u3.csv");
-writematrix(horzcat(frequencies', maxima_u1'), "u1.csv");
-writematrix(horzcat(frequencies', maxima_u2'), "u2.csv");
-writematrix(horzcat(frequencies', maxima_u3'), "u3.csv");
+writematrix(horzcat(frequencies', maxima_u1_u2_u3'), "maxima_u1_u2_u3.csv");
+writematrix(horzcat(frequencies', maxima_u1'), "maxima_u1.csv");
+writematrix(horzcat(frequencies', maxima_u2'), "maxima_u2.csv");
+writematrix(horzcat(frequencies', maxima_u3'), "maxima_u3.csv");
+writematrix(u_3, "u3.csv");
